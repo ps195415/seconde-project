2ecc70db9f284383e3f7198d2e41fe29b9f8910d
ps195415 <ps195415@edu.summacollege.nl>
2021-03-29 17:53:39 +0200

    fiets pagina afgerond

---
+++ web/javascript/main.js
@@ -0,0 +1,140 @@
+function openNav() {
+  document.getElementById('MysidenarBar').style.width = "100%";
+  document.getElementById('header').style.display = "none";
+  document.getElementById('topNav').style.display = "none";
+  document.getElementById('main').style.display = "none";
+}
+
+function ColseBtn() {
+  document.getElementById('MysidenarBar').style.width = "0%";
+  document.getElementById('header').style.display = "block";
+  document.getElementById('topNav').style.display = "block";
+  document.getElementById('main').style.display = "block";
+}
+
+
+// dit is voor het foto galarij in de home page.
+/*var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+//hier komt het openings tijd van het home page 
+var d = new Date();
+
+
+function Open_dicht() {
+
+}*/
+
+
+
+
+function ImgGroter() {
+
+  document.getElementById('card2').style.display = 'none';
+  document.getElementById('card3').style.display = 'none';
+  document.getElementById('card4').style.display = 'none';
+  document.getElementById('card1').style.height = '450px';
+  document.getElementById('cardimg1').style.height = '40%';
+  document.getElementById('infoCard1').style.height = '50%';
+  document.getElementById('btnImg1').style.height = '10%';
+
+}
+
+function kleiner(){
+  document.getElementById('card2').style.display = 'block';
+  document.getElementById('card3').style.display = 'block';
+  document.getElementById('card4').style.display = 'block';
+  document.getElementById('card1').style.height = '300px';
+  document.getElementById('cardimg1').style.height = '70%';
+  document.getElementById('infoCard1').style.height = '15%';
+  document.getElementById('btnImg1').style.height = '10%';
+}
+
+
+
+function ImgGroter2() {
+
+  document.getElementById('card1').style.display = 'none';
+  document.getElementById('card3').style.display = 'none';
+  document.getElementById('card4').style.display = 'none';
+  document.getElementById('card2').style.height = '450px'
+  document.getElementById('cardimg2').style.height = '40%';
+  document.getElementById('infoCard2').style.height = '50%';
+  document.getElementById('btnImg2').style.height = '10%';
+}
+
+function kleiner2() {
+  document.getElementById('card1').style.display = 'block';
+  document.getElementById('card3').style.display = 'block';
+  document.getElementById('card4').style.display = 'block';
+  document.getElementById('card2').style.height = '300px'
+  document.getElementById('cardimg2').style.height = '70%';
+  document.getElementById('infoCard2').style.height = '15%';
+  document.getElementById('btnImg2').style.height = '10%';
+}
+
+function ImgGroter3() {
+
+  document.getElementById('card1').style.display = 'none';
+  document.getElementById('card2').style.display = 'none';
+  document.getElementById('card4').style.display = 'none';
+  document.getElementById('card3').style.height = '450px'
+  document.getElementById('cardimg3').style.height = '40%';
+  document.getElementById('infoCard3').style.height = '50%';
+  document.getElementById('btnImg3').style.height = '10%';
+}
+
+function kleiner3() {
+  document.getElementById('card1').style.display = 'block';
+  document.getElementById('card2').style.display = 'block';
+  document.getElementById('card4').style.display = 'block';
+  document.getElementById('card3').style.height = '300px'
+  document.getElementById('cardimg3').style.height = '70%';
+  document.getElementById('infoCard3').style.height = '15%';
+  document.getElementById('btnImg3').style.height = '10%';
+}
+
+function ImgGroter4() {
+
+  document.getElementById('card1').style.display = 'none';
+  document.getElementById('card3').style.display = 'none';
+  document.getElementById('card2').style.display = 'none';
+  document.getElementById('card4').style.height = '450px'
+  document.getElementById('cardimg4').style.height = '40%';
+  document.getElementById('infoCard4').style.height = '50%';
+  document.getElementById('btnImg4').style.height = '10%';
+}
+
+function kleiner4() {
+  document.getElementById('card1').style.display = 'block';
+  document.getElementById('card3').style.display = 'block';
+  document.getElementById('card2').style.display = 'block';
+  document.getElementById('card4').style.height = '300px'
+  document.getElementById('cardimg4').style.height = '70%';
+  document.getElementById('infoCard4').style.height = '15%';
+  document.getElementById('btnImg4').style.height = '10%';
+}
